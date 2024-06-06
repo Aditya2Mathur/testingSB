e7daf50a91c2b82640d4b63df8f057ce0652d773
Aditya Mathur <adiasfreelancer@gmail.com>
2024-06-06 19:25:28 +0530

    fetch code od 3rd commit

--- js/data.js
+++ js/data.js
@@ -38,7 +38,7 @@ fetch('../data/services.json')
 
 //  5 services for index page
 // data.js
-fetch('../data/services.json')
+fetch('data/services.json')
     .then(response => response.json())
     .then(data => {
         const services = data.services.slice(0, 5); // Get only the top 5 services
@@ -71,61 +71,29 @@ fetch('../data/services.json')
     .catch(error => console.error('Error fetching services data:', error));
 
     // Blogs for blog page
-	fetch('../data/blog.json')
-			.then(response => response.json())
-			.then(data => {
-				const blogCardsContainer = document.getElementById('blogCards');
-				const paginationContainer = document.getElementById('pagination');
-				const blogsPerPage = 6;
-				let currentPage = 1;
-	
-				function displayBlogCards(page) {
-					blogCardsContainer.innerHTML = '';
-					const startIndex = (page - 1) * blogsPerPage;
-					const endIndex = startIndex + blogsPerPage;
-					const currentBlogs = data.blogs.slice(startIndex, endIndex);
-	
-					currentBlogs.forEach(blog => {
-						const blogCard = `
-							<div class="col-lg-4 col-md-6">
-								<div class="post-item wow fadeInUp" data-wow-delay="0.25s">
-									<div class="post-featured-image">
-										<figure class="image-anime">
-											<a href="${blog.read_more}"><img src="${blog.img}" alt=""></a>
-										</figure>
-									</div>
-									<div class="post-item-body">
-										<h2><a href="${blog.read_more}">${blog.title}</a></h2>
-										<p>${blog.content}</p>
-									</div>
-									<div class="btn-readmore">
-										<a href="${blog.read_more}">Read More <i class="fa-solid fa-arrow-right-long"></i></a>
-									</div>
-								</div>
-							</div>
-						`;
-						blogCardsContainer.innerHTML += blogCard;
-					});
-				}
-	
-				function setupPagination() {
-					const totalPages = Math.ceil(data.blogs.length / blogsPerPage);
-					let paginationButtons = '';
-					for (let i = 1; i <= totalPages; i++) {
-						paginationButtons += `<button onclick="displayBlogCards(${i})">${i}</button>`;
-					}
-					paginationContainer.innerHTML = paginationButtons;
-				}
-	
-				setupPagination(); // Call setupPagination initially
-	
-				displayBlogCards(currentPage); // Call displayBlogCards with initial page
-	
-				// Update pagination and display cards when a pagination number is clicked
-				paginationContainer.addEventListener('click', (event) => {
-					if (event.target.tagName === 'BUTTON') {
-						currentPage = parseInt(event.target.textContent);
-						displayBlogCards(currentPage);
-					}
-				});
-			});
+    fetch('data/blog.json')
+    .then(response => response.json())
+    .then(data => {
+        const blogCardsContainer = document.getElementById('blogCards');
+        data.blogs.forEach(blog => {
+            const blogCard = `
+                <div class="col-lg-4 col-md-6">
+                    <div class="post-item wow fadeInUp" data-wow-delay="0.25s">
+                        <div class="post-featured-image">
+                            <figure class="image-anime">
+                                <a href="${blog.read_more}"><img src="${blog.img}" alt=""></a>
+                            </figure>
+                        </div>
+                        <div class="post-item-body">
+                            <h2><a href="${blog.read_more}">${blog.title}</a></h2>
+                            <p>${blog.content}</p>
+                        </div>
+                        <div class="btn-readmore">
+                            <a href="${blog.read_more}">Read More <i class="fa-solid fa-arrow-right-long"></i></a>
+                        </div>
+                    </div>
+                </div>
+            `;
+            blogCardsContainer.innerHTML += blogCard;
+        });
+    });
